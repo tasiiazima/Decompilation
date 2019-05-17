8d7cff076ae14217f1c85583f0e9151d337d4ddf
tasiiazima <50640377+tasiiazima@users.noreply.github.com>
2019-05-17 23:58:50 +0300

    add docunentation

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -10,12 +10,13 @@ extends JFrame {
     public TitlesFrame() {
         this.initUI();
     }
-
+/** задаем параметры фигуры(заголовок, фигуру и цвет по варианту, размер) */
     private void initUI() {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
         this.add(new TitlesPanel(37));
         this.setSize(350, 350);
+        /**центрируем*/
         this.setLocationRelativeTo(null);
     }
 
